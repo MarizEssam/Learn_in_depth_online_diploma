2e266cb0e925956aa1bc255719a492aa33f31fdf
Mariz <marizessam@gmail.com>
2023-12-16 13:51:16 +0200

    1st assignment

---
+++ c Programming/assign1-1/src/assign1-1.c
@@ -0,0 +1,17 @@
+/*
+ ============================================================================
+ Name        : assign1-1.c
+ Author      : Mariz
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+	puts("C programming"); /* prints !!!Hello World!!! */
+	return 0;
+}
